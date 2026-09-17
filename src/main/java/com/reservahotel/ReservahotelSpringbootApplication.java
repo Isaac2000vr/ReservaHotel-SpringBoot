01e8e89cd49c517c350f496354b93d83ec430ec5
Isaac Vasquez <ivasquezr@unicartagena.edu.co>
2026-09-16 23:59:47 -0500

    feat: subo la estructura inicial del proyecto spring boot y la configuración base

---
+++ src/main/java/com/reservahotel/ReservahotelSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.reservahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservahotelSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservahotelSpringbootApplication.class, args);
+    }
+
+}
